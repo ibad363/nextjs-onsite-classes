5a5cda0c584caa6026bd3d742d055742e08bccff
Ibad Ur Rehman <HAFIZIBADURREHMAN363@GMAIL.COM>
2024-10-09 12:49:07 +0500

    class-04 rendering theory

---
+++ class-04/app/page.tsx
@@ -0,0 +1,9 @@
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <div>
+      <h1>In Class 04, All Class Content About Rendering Theory in Next JS</h1>
+    </div>
+  );
+}
