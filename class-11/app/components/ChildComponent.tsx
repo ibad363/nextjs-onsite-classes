35132907da5e1eb93162f06aaad473b3a5410b17
Ibad <HAFIZIBADURREHMAN363@GMAIL.COM>
2024-11-27 10:35:01 +0500

    styling update

--- class-11/app/components/ChildComponent.tsx
+++ class-11/app/components/ChildComponent.tsx
@@ -1,8 +1,8 @@
 const ChildComponent = (props:any) => {
   return (
     <div className="border-2 border-gray-600 mb-5 p-2">
-        <h1 className="text-4xl mb-2">My Favorite Color is {props.color}</h1>
-        <h1 className="text-4xl">My Favorite Dish is {props.dish}</h1>
+        <h1 className="text-lg sm:text-xl md:text-4xl mb-2">My Favorite Color is {props.color}</h1>
+        <h1 className="text-lg sm:text-xl md:text-4xl">My Favorite Dish is {props.dish}</h1>
     </div>
   )
 }
--- class-11/app/page.tsx
+++ class-11/app/page.tsx
@@ -4,7 +4,7 @@ import FeatureCar from "./components/FeatureCar";
 
 export default function Home() {
   return (
-    <div className="grid items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
+    <div className="grid items-center justify-items-center min-h-screen p-8 pb-20 gap-2 font-[family-name:var(--font-geist-sans)]">
       <ParentComponent/>
       <FeatureCar/>
     </div>
